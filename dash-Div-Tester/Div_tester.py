79dd16c908b5c9e5f7d5d91fd060894f3e18802d
Tobias Legenden <tobiaslegenden@gmail.com>
2022-07-21 01:29:34 +0200

    added: dash-div-tester

--- dash-Div-Tester/Div_tester.py
+++ dash-Div-Tester/Div_tester.py
@@ -4,6 +4,20 @@ from dash import html, dash
 app = dash.Dash(__name__)
 
 # Creating the Div layout
+app.layout = html.Div([
+    html.H1('Main Div.', style={'textAlign': 'center', 'color': 'white'}),
+
+    html.Br(),
+
+    html.Div([
+        html.H2('First div.', style={'color': 'white'}),
+        html.P('Blah blah blah blah.', style={'color': 'white'})
+    ], className='card_container four columns', style={'background-color': '#1f2c56'}),
+
+    html.Div([
+        html.H2('Second div.', style={'color': 'white'})
+    ], className='card_container four columns', style={'background-color': '#1f2c56'})
+])
 
 
 if __name__ == '__main__':
